95f892638b01f770a55c89562a5735fe550e1ae3
Joshe1601 <112984569+Joshe1601@users.noreply.github.com>
2022-11-15 21:20:36 -0500

    Add files via upload

---
+++ pregunta2_lab9_plantilla_v2.py
@@ -0,0 +1,32 @@
+
+rutas_posibles = [
+	"LIM_PIU_CAJ",
+	"LIM_PIU_CHI_HUA_CAJ",
+	"LIM_PIU_CHI_OXA_HUA_CAJ",
+	"LIM_OXA_CHI_PIU_CAJ",
+	"LIM_OXA_CHI_HUA_CAJ",
+	"LIM_OXA_HUA_CAJ",
+	"LIM_OXA_HUA_CHI_PIU_CAJ",
+	"LIM_CHI_PIU_CAJ",
+	"LIM_CHI_OXA_HUA_CAJ",
+	"LIM_CHI_HUA_CAJ"
+]
+
+distancias = {
+	"LIM_PIU": 14,
+	"LIM_CHI": 10,
+	"LIM_OXA": 8,
+	"PIU_CAJ": 18,
+	"PIU_CHI": 3,
+	"OXA_CHI": 25,
+	"OXA_HUA": 5,
+	"CHI_PIU": 3,
+	"CHI_OXA": 25,
+	"CHI_HUA": 2,
+	"HUA_CHI": 2,
+	"HUA_CAJ": 10
+}
+
+if __name__ == "__main__":
+	"""Escriba su código aquí"""
+	pass
